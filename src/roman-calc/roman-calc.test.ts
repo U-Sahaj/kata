64271b3c5c124d46afd64c328e3878f207a632ea
U-Sahaj <yeuwenm@sahaj.ai>
2023-03-02 19:31:54 +1100

    Implemented simple addition such as I + I = II

--- src/roman-calc/roman-calc.test.ts
+++ src/roman-calc/roman-calc.test.ts
@@ -2,16 +2,28 @@ import { add } from "./roman-calc"
 
 describe("roman-calc", () => {
     it("should return error given IIII", () => {
-        expect(add("IIII","II")).toBe("Invalid Numeral")
+        expect(add("IIII","II")).toBe("Invalid First Numeral")
     })
-    it("should return error given XXXX", () => {
-        expect(add("II","IXXXX")).toBe("Invalid Numeral")
+    it("should return error given IXXXX", () => {
+        expect(add("II","IXXXX")).toBe("Invalid Second Numeral")
     })
-    it("should return error given IILL ", () => {
-        expect(add("II","IILL")).toBe("Invalid Numeral")
+    it("should return error given IILL", () => {
+        expect(add("II","IILL")).toBe("Invalid Second Numeral")
     })
-    // it("should return XXII given XX and II ", () => {
-    //     expect(add("XX,II")).toBe("XXII")
-    
+    it("should return 2 given I and I", () => {
+        expect(add("I","I")).toBe("2")
+    })
+    it("should return 4 given II and II", () => {
+        expect(add("II","II")).toBe("4")
+    })
+    it("should return 101 given I and C", () => {
+        expect(add("I","C")).toBe("101")
+    })
+    it("should return 30 given XX and X", () => {
+        expect(add("XX","X")).toBe("30")
+    })
+    // it("should return   given IV and IIV", () => {
+    //     expect(add("IV","IIV")).toBe("")
     // })
+
 })    
--- src/roman-calc/roman-calc.ts
+++ src/roman-calc/roman-calc.ts
@@ -3,20 +3,24 @@
 
 export function add(numeral1:string, numeral2:string) : string {
 // Implement https://www.math-only-math.com/rules-for-formation-of-roman-numerals.html
+// https://www.numere-romane.ro/example-how-to-add-roman-numerals-steps-explanations.php?lang=en
 // 1. Transform Roman numerals into their corresponding decimals
 // 2. Perform addition
 // 3. Transform the decimal answer to Roman numerals
 
-    if (!isValid(numeral1) || !isValid(numeral2)) {
-        return "Invalid Numeral"
+    if (!isValid(numeral1)) {
+        return "Invalid First Numeral"
+    }
+    if (!isValid(numeral2)) {
+        return "Invalid Second Numeral"
     }
 
-// split
+    const value = doRomanToDecimal(numeral1) + doRomanToDecimal(numeral2)
 
     
 
-
-    return "Valid Numerals"
+    // return "Valid Numeral"
+    return `${value}`
 }
 
 function toDecimal(numeral:string) {
@@ -29,21 +33,50 @@ function isValid(aNumeralString:string) : boolean {
     const numerals = ["I", "V", "X", "L", "C", "D", "M"]
     const decimals = [1, 5, 10, 50, 100, 500, 1000]
 
-    const invalidNumerals = ["IIII", "XXXX", "CCCC", "VV", "LL", "DD"]
+    const invalidNumerals = ["IIII", "XXXX", "CCCC", "VV", "LL", "DD", "VX"]
 
-    let isValid = true
-    isValid = invalidNumerals.includes(aNumeralString)
-    if (!isValid) { return false }
+    for (let index = 0; index < invalidNumerals.length; index++) {
+        if (aNumeralString.includes(invalidNumerals[index])) {
+            return false
+        }   
+    }
 
-    // for (let index = 0; index < invalidNumerals.length; index++) {
-    //     isValid = !aNumeralString.includes(invalidNumerals[index])
-    //     if (!isValid) { return false }
-    // }
-
-    // for (let index = 0; index < aNumeralString.length; index++) {
-    //     const element = aNumeralString[index];
-        
-    // }
-    // numerals.indexOf
     return true
+}
+
+function doRomanToDecimal(aNumeralString:string) : number {
+console.log(`aNumeralString is ${aNumeralString}`)
+    const numerals = ["I", "V", "X", "L", "C", "D", "M"]
+    const decimals = [1, 5, 10, 50, 100, 500, 1000]
+    let value = 0
+    let index = 0
+    do {
+        const theNumeral = numerals.indexOf(aNumeralString[index])
+        value = value + decimals[theNumeral]
+console.log(`value is ${value} when index is ${index}`)
+        index += 1
+    } while (index < aNumeralString.length);
+
+    // let aDecimalString = []
+    // for (let index = 0; index < aNumeralString.length-1; index++) {
+    // let index = 0
+    // do {
+        
+    //     const theNumeral = numerals.indexOf(aNumeralString[index])
+    //     const theNumeralOnTheRight = numerals.indexOf(aNumeralString[index+1])
+    //     const theNumeralOnTheLeft = 
+    //     if (decimals[whichNumeral1] < decimals[whichNumeral2]){
+    //         value = value + decimals[whichNumeral2] - decimals[whichNumeral1]
+    //         //remove both numerals from aNumeralString
+    //         //aNumeralString.
+
+    //     } else {
+    //     }
+
+    // } while (index < aNumeralString.length-1);
+    
+
+
+
+    return value
 }
